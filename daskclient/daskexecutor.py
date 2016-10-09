9f180f91ae79d0cd695b3f6dee4542d1109934a2
Matt Keagle <keaglem@gmail.com>
2016-10-09 17:31:38 -0400

    Cleaning up the code to add error checking.  Updating display to display current status and error conditions.

--- daskclient/daskexecutor.py
+++ daskclient/daskexecutor.py
@@ -1,4 +1,5 @@
 from distributed import Executor, Scheduler, Nanny, Worker
+import distributed
 from tornado.ioloop import IOLoop
 from threading import Thread
 import taskclient
@@ -14,45 +15,23 @@ HOME_PAGE = 'http://localhost:5050'
 def test(one, two) :
     return 4
 
-
-
-print('Hello')
-loop_worker = IOLoop.current()
-t = Thread(target=loop_worker.start)
-t.start()
-loop_worker2 = IOLoop.current()
-t2 = Thread(target=loop_worker2.start)
-t2.start()
-nanny_process = Worker(center_ip=SCHEDULER_IP,
-                       center_port=SCHEDULER_PORT,
-                       loop=loop_worker,
-                       ncores=1)
-nanny_process.start()
-
-nanny_process2 = Worker(center_ip=SCHEDULER_IP,
-                       center_port=SCHEDULER_PORT,
-                       loop=loop_worker2,
-                       ncores=1)
-nanny_process2.start()
-print('Im here')
-
-
 executor = Executor('{}:{}'.format(SCHEDULER_IP, SCHEDULER_PORT))
-#
 
 taskclient.add_user(HOME_PAGE)
-for _ in range(10):
+for _ in range(1):
     taskclient.add_job(HOME_PAGE)
 
-for _ in range(5):
-    result = executor.map(taskclient.worker, [HOME_PAGE, HOME_PAGE], [0, 1])
-    sim_results = executor.gather(result)
-    print([x['node_id'] for x in sim_results])
-    time.sleep(2)
+result_list = []
+for test in range(5):
+    #result = executor.map(taskclient.worker, [HOME_PAGE, HOME_PAGE], [0, 1])
+    result = executor.submit(taskclient.worker, HOME_PAGE, test)
+    #executor.submit(result)
+    result_list.append(result)
 
-nanny_process.stop()
-nanny_process2.stop()
-loop_worker.stop()
-loop_worker2.stop()
-t.join()
-t2.join()
+distributed.diagnostics.progress(result_list)
+sim_results = executor.gather(result_list)
+print('---------')
+print(sim_results[0] is sim_results[1])
+print('---------')
+print([x['node_id'] for x in sim_results])
+
