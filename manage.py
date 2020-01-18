4c44a5be116eb605a4fb910f3e3b932bef1b0c35
Matthew Keagle <keaglem@gmail.com>
2020-01-18 11:25:03 -0500

    Adding to work with visual studio

--- manage.py
+++ manage.py
@@ -21,6 +21,7 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
+    print(f'Initializing database')
     import taskapp.models
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
